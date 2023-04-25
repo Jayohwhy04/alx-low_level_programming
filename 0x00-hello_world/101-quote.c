360c78afb3aeb64c748aed10ffa176fcc779491a
joyedemevughe <joyedemevughe@gmail.com>
2023-04-24 21:11:36 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - print piece of art
+ * Return: (1)
+ */
+int man(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+}
