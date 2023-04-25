bf1c2300021a85bde3835180371e20b2d56f9ca8
joyedemevughe <joyedemevughe@gmail.com>
2023-04-25 15:34:43 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabets in lowercase
+ */
+void print_alphabet(void)
+{
+	char le;
+
+	for (le = 'a'; le <= 'z'; le++)
+		_putchar(le);
+
+	_putchar('\n');
+}
