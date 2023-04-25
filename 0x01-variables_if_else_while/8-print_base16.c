dafeefc8586024f581ec0692617e8dc0b6abb57c
joyedemevughe <joyedemevughe@gmail.com>
2023-04-24 17:13:45 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print base 16
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	char la;
+
+	for (num = 0; number < 10; num++)
+		putchar(num + '0');
+
+	for (la = 'a'; la <= 'f', la++)
+		putchar(la);
+
+	putchar('\n');
+
+	return (0);
+}
