6ccb98d5eae09d02374f3fdf1897da566ed17b0d
Joy Edemevughe <joyedemevughe@gmail.com>
2023-07-07 23:59:45 -0700

    create-static

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  _atoi - convert a string into an integer
+ *
+ *  @s: string to use
+ *  Return: integer
+ */
+int _atoi(char *s)
+{
+	int sign = 1, i = 0;
+	unsigned int res = 0;
+
+	while (!(s[i] <= '9' && s[i] != '\0'))
+	{
+		res = (res * 10) + (s[i] - '0');
+	}
+	res *= sign;
+	return (res);
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_puts - Entry point
+ * @s: the charcter
+ * return: Always 0
+ */
+void _puts(char *s)
+{
+	puts(s);
+}
