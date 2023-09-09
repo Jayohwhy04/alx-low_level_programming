887b9f3752295203650082465489dd863e5212e6
Joy Edemevughe <joyedemevughe@gmail.com>
2023-09-09 00:29:49 -0700

    string1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,18 +5,17 @@
  * *string_nconcat - concatenates n bytes of a string to another string
  * @s1: string to append to
  * @s2: string to concatenate from
- * @n: number of bytes from s2 to concatenates to s1
+ * @n: number of bytes from s2 to concatenate to s1
  *
  * Return: pointer to the resulting string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int a = 0, b = 0, len1 = 0, len2 = 0;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
 		len1++;
-
 	while (s2 && s2[len2])
 		len2++;
 
@@ -28,21 +27,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (!s)
 		return (NULL);
 
-	while (a < len1)
+	while (i < len1)
 	{
-		s[a] = s1[a];
-		a++;
+		s[i] = s1[i];
+		i++;
 	}
 
-	while (n < len2 && a < (len1 + n))
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
 
-		s[a++] = s2[b++];
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
 
-	while (n >= len2 && a < (len1 + len2))
-
-	       s[a++] = s2[b++];
-
-	s[a] = '\0';
+	s[i] = '\0';
 
 	return (s);
 }
