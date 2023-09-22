9c3553fe6cdfd8fe1b36ef8ede7a74167ef2125c
Joy Edemevughe <joyedemevughe@gmail.com>
2023-09-21 21:52:00 -0700

    0prilis

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - print all the elements of a linked list
+ * @h: pointer
+ *
+ * Return: Number of nodes printed
+ */
+size_t print_list(const list_t *h)
+{
+	size_t s = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
+	}
+	return (s);
+}
